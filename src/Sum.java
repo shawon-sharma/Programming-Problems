b567a3357709f79ef07ff9721420e27648e62858
shawon-sharma <dmission60@gmail.com>
2022-11-18 16:11:33 +0100

    sum of two valuues

---
+++ src/Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args){
+        int value1=10;
+        int value2=20;
+        int sum = value1 + value2;
+
+        System.out.println(" summation is: "+sum);
+    }
+}
